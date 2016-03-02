65a818b66fa31025e00cb06e7aedc63b03358801
Xwh <1241361935@qq.com>
2016-03-02 18:47:49 +0800

    init

---
+++ src/com/example/asyncimageloader/Images.java
@@ -0,0 +1,49 @@
+package com.example.asyncimageloader;
+
+public class Images {
+
+	public final static String[] imageThumbUrls = new String[] {
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg",
+			"http://pica.nipic.com/2007-11-09/200711912453162_2.jpg"
+			};
+	
+}
